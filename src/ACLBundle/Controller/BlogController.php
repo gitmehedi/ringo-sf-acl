48e595a809cbf6ced78b2676aa0e584ce9ec8207
gitmehedi <git.mehedi@gmail.com>
2015-03-18 19:08:03 +0600

    add blogContoler

---
+++ src/ACLBundle/Controller/BlogController.php
@@ -0,0 +1,39 @@
+<?php
+namespace ACLBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+
+class BlogController extends Controller
+{
+    // ...
+
+    public function addCommentAction(Post $post)
+    {
+        $comment = new Comment();
+
+        // ... setup $form, and submit data
+
+        if ($form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            // creating the ACL
+            $aclProvider = $this->get('security.acl.provider');
+            $objectIdentity = ObjectIdentity::fromDomainObject($comment);
+            $acl = $aclProvider->createAcl($objectIdentity);
+
+            // retrieving the security identity of the currently logged-in user
+            $securityContext = $this->get('security.context');
+            $user = $securityContext->getToken()->getUser();
+            $securityIdentity = UserSecurityIdentity::fromAccount($user);
+
+            // grant owner access
+            $acl->insertObjectAce($securityIdentity, MaskBuilder::MASK_OWNER);
+            $aclProvider->updateAcl($acl);
+        }
+    }
+}
